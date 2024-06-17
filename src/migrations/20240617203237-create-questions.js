9b92283a7960ac46d341684cfe42edb19b2481d4
CodeUpSubodh <subodhonwork@gmail.com>
2024-06-18 02:07:49 +0530

    Implement Question Model

---
+++ src/migrations/20240617203237-create-questions.js
@@ -0,0 +1,26 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Questions', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('questions');
+  }
+};
---
+++ src/models/Questions.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../database/database');
+const User = require('./User');
+
+const Question = sequelize.define('Question', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: {
+        msg: 'Content is required'
+      }
+    }
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  }
+});
+
+Question.sync();
+
+module.exports = Question;
